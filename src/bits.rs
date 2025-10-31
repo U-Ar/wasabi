86d6a58bbf1a952444f8df4ebe7cbac3d9aa8796
U-Ar <yuma.arakawa82128awakara.amuy@gmail.com>
2025-10-31 14:14:28 +0900

    extract bits

--- src/bits.rs
+++ src/bits.rs
@@ -11,6 +11,22 @@ where
     TryInto::try_into(value).unwrap_or_else(|_| T::from(0u8))
 }
 
+pub fn extract_bits_from_le_bytes(bytes: &[u8], shift: usize, width: usize) -> Option<u64> {
+    if width == 0 {
+        return None;
+    }
+    let byte_range = shift / 8..(shift + width).div_ceil(8);
+    let mut value = 0u64;
+    let bit_shift = shift - byte_range.start * 8;
+    bytes.get(byte_range).map(|bytes_in_range| {
+        for (i, v) in bytes_in_range.iter().enumerate() {
+            let v = *v as u128;
+            value |= ((v << (i * 8)) >> bit_shift) as u64;
+        }
+        extract_bits(value, 0, width)
+    })
+}
+
 #[test_case]
 fn extract_bits_tests() {
     assert_eq!(extract_bits(30u32 << 24, 24, 8), 30u32);
@@ -28,5 +44,28 @@ fn extract_bits_tests() {
     assert_eq!(
         extract_bits(0x1234_5678_1234_5678u64, 0, 65),
         0x1234_5678_1234_5678u64
+    );
+}
+
+#[test_case]
+fn extract_bits_from_le_bytes_tests() {
+    assert_eq!(extract_bits_from_le_bytes(&[], 0, 0), None);
+    assert_eq!(extract_bits_from_le_bytes(&[], 0, 1), None);
+    assert_eq!(extract_bits_from_le_bytes(&[], 1, 0), None);
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 0, 0),
+        None
+    );
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 0, 8),
+        Some(0b01010101)
+    );
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 8, 8),
+        Some(0b10101010)
+    );
+    assert_eq!(
+        extract_bits_from_le_bytes(&[0b01010101, 0b10101010], 4, 8),
+        Some(0b10100101)
     );
 }
